fa8a2f4cf4769e94de74ca275a12741fa74ac35d
Madhav Suresh <madman92@gmail.com>
2013-03-06 18:40:01 -0600

    main web app added get to run videos locally

--- viewem.py
+++ viewem.py
@@ -1,34 +1,31 @@
 import web
-from time import strptime, strftime
 from threading import Thread
-from subprocess import Popen
 import os
 
+PLAYER = 'omxplayer'
+VIDEO_PATH = '/home/pi/stim/static/vids'
 
-urls = ('/(\w*)', 'play', '/playem/(\w*)',
+urls = ('/', 'play', '/playem',
         'playit')
-render = web.template.render('/home/pi/stim2/templates/')
+render = web.template.render('/home/madhav/bme390/stim/templates/')
 app = web.application(urls, globals())
 class play:
-    def GET(self,arg):
-        return render.resp('')
+    def GET(self):
+        print 'wat?'
+        return render.out()
 
 class playit:
-    def GET(self,arg):
-        t = Thread(target=play_em)
-        t.start()
-        return render.resp('hello world')
-        
-
-class addTodo:
-    def POST(self, school):
+    def GET(self):
         i = web.input()
-        with open('./todo', 'a') as tdo:
-                tdo.write(i['todo1'] + '\n')
-
-
-def play_em():
- os.system('sleep 1 && omxplayer /home/pi/out.mp4')
+        t = Thread(target=play_em, args=[i['file']])
+        t.start()
+        return None
+       
+        
+def play_em(fname):
+    print fname
+    exstr = PLAYER + ' ' + VIDEO_PATH + ' ' + fname
+    os.system(exstr)
 
 if __name__ == '__main__':
     app.run()
