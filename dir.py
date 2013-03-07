00c3b53c9a52b391484b70d171b442e405010464
Madhav Suresh <madman92@gmail.com>
2013-03-06 18:39:26 -0600

    added video files, templator

--- dir.py
+++ dir.py
@@ -1,4 +1,5 @@
 import os
+from jinja2 import Template
 
 SUFFIX_LENGTH = 4
 
@@ -6,20 +7,28 @@ def get_categories(directory):
     cats = {}
     l = os.listdir(directory)
     for vid in l:
-        spl = vid.split('#')
-        print spl
+        spl = vid.split('__')
         pretty_print = spl[1][:-SUFFIX_LENGTH].replace('_',' ')
-        print pretty_print
-        
         if len(spl) > 1 and spl[0] in cats:
             cats[spl[0]].append((vid,pretty_print))
         else:
             cats[spl[0]] = [(vid,pretty_print),]
+    '''
     for key in cats:
         print key
         for els in cats[key]:
             print '\t' , els[0] + ' ' +  els[1]
+            '''
     return cats
-        
 
-get_categories('./vids/')
+def template_dis(cats,fname):
+    f = open(fname)
+    c = f.read()
+    template = Template(c)
+    return template.render(vids = cats)
+
+
+f = open('out.html','w')
+s = template_dis(get_categories('./vid_bk/'),'./temps/tst.html')
+f.write(s)
+f.close()
